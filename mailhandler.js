b3d54a1cdc54ffda085c3dc9f8e8895a9fdf04dc
Scott Robinson <srobin@mit.edu>
2015-04-24 03:16:41 -0400

    better organization

--- mailhandler.js
+++ mailhandler.js
@@ -1,150 +1,29 @@
 var nodemailer = require('nodemailer'),
-	fs = require('fs'),
-	//var body = fs.readFileSync("body.html", "utf8");
-	path = require('path'),
-	session = require('express-session'),
-	fs = require('fs'),
-	MailParser = require("mailparser").MailParser,
-	Mbox = require('node-mbox'),
-	mongoose = require('mongoose'),
-	async = require('async'),
-	crypto = require('crypto'),
-	animal = require('animal-id');
+    fs = require('fs'),
+    //var body = fs.readFileSync("body.html", "utf8");
+    path = require('path'),
+    session = require('express-session'),
+    fs = require('fs'),
+    MailParser = require("mailparser").MailParser,
+    Mbox = require('node-mbox'),
+    mongoose = require('mongoose'),
+    async = require('async'),
+    crypto = require('crypto'),
+    animal = require('animal-id');
 
 animal.useSeparator(' ');
 
 var Pseudonym = require('./models/pseudonym').Pseudonym,
-	Pseudohash = require('./models/pseudohash')
-	
+    Pseudohash = require('./models/pseudohash')
+    
 
 mongoose.connect('mongodb://localhost/6857', function(err) {
-	if(err) throw err;
+    if(err) throw err;
 })
 
 if (process.getuid() != 0) {
-	throw "You must run the mail parser as root!"
+    throw "You must run the mail parser as root!"
 }
-
-
-
-var mbox
-
-fs.watchFile('/var/mail/bob',function(curr,prev){
-	mbox = new Mbox('/var/mail/bob',{});
-
-	mbox.on('message', function(msg) {
-        	mbox.lastm = msg
-	//      console.log('got a message', msg);
-	//      mailparser.write(msg);
-	//      mailparser.end();
-	})
-
-	mbox.on('end', function() {
-		var mailparser = new MailParser();
-			
-		mailparser.on("end", function(mail_object){
-        		var sender = mail_object.from[0].address
-        		var subject = mail_object.subject
-			var query_string = subject.split("||")[subject.split("||").length-1]
-			var qs = parseQS(query_string)
-			if(subject.indexOf("||") != -1)
-				subject = subject.substring(0,subject.lastIndexOf("||"))
-			console.log(subject)
-			console.log(query_string)
-			var p = mail_object.to[0].name
-			console.log(sender)
-			console.log(mail_object.to)
-			var message = {}
-			message.to = []
-			message.subject = subject
-			message.body = mail_object.text
-			if(!message.body)
-				message.body = mail_object.html
-			console.log(p)				
-			if(!p || p == ""){
-				//new conversation (not replying)
-				if(!qs.to || qs.to == "")
-					return
-				
-				Pseudonym.findOne({initiator:sender,receiver:qs.to}).exec(function(err,pseudonym){
-					if(err)
-						throw err
-					if(pseudonym){
-						message.from = pseudonym.initiator_pseudonym 
-						message.to.push(qs.to)
-						return send(message)
-					}
-					else{
-						Pseudonym.findOne({initiator:qs.to,receiver:sender}).exec(function(err,pseudonym){
-							if(err)
-								throw err
-							if(pseudonym){
-								message.from = pseudonym.receiver_pseudonym
-								message.to.push(qs.to)
-								return send(message)
-							}
-							else{
-								var i_pseudo = animal.getId();
-								var r_pseudo = animal.getId();
-
-								var newPseudo = new Pseudonym({})
-								newPseudo.initiator = sender
-								newPseudo.initiator_pseudonym = i_pseudo
-								newPseudo.receiver = qs.to
-								newPseudo.receiver_pseudonym = r_pseudo
-								newPseudo.save(function(err,nps){
-									if(err)
-										throw err
-									message.from = nps.initiator_pseudonym
-									message.to.push(qs.to)
-									return send(message)
-								})
-							}
-						})
-					}
-				})
-			}
-			else{
-				//replying
-				if(!p || p == "")
-					return
-
-				Pseudonym.findOne({initiator:sender,receiver_pseudonym:p}).exec(function(err,pseudonym){
-					if(err)
-						throw err
-					if(pseudonym){
-						if(qs.destroy == 'true')
-							return destroy(pseudonym)
-						message.from = pseudonym.initiator_pseudonym
-						message.to.push(pseudonym.receiver)
-						return send(message)
-					}
-					else{
-						Pseudonym.findOne({receiver:sender,initiator_pseudonym:p}).exec(function(err,pseudonym){
-							if(err)
-								throw err
-							if(pseudonym){
-								if(qs.destroy == 'true')
-									return destroy(pseudonym)
-				                                message.from = pseudonym.receiver_pseudonym
-                                		                message.to.push(pseudonym.initiator)
-								return send(message)
-							}
-							else{
-								return //they borked the pseudonym
-							}
-						})
-					}
-				})
-			}
-		});
-
-
-        	mailparser.write(mbox.lastm)
-        	mailparser.end()
-		mbox = {}
-	});
-})
 
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
@@ -153,39 +32,169 @@ var transporter = nodemailer.createTransport({
     tls: {rejectUnauthorized: false}
 });
 
-function parseQS(query_string){
-	var qs = {}
-	var parts = query_string.split(";")
-	parts.forEach(function(p){
-		var keyval = p.split(":")
-		//var val
-		//if(keyval[1].indexOf(",") != -1){
-		//	val = keyval[1].split(",")
-		//}
-		//else
-		//	val = keyval[1]
-		qs[keyval[0]] = keyval[1]
-	})
+//begin execution with this call
+watch();
 
-	return qs
+function watch(){
+    var mbox
+
+    fs.watchFile('/var/mail/bob',function(curr,prev){
+        mbox = new Mbox('/var/mail/bob',{});
+
+        mbox.on('message', function(msg) {
+                mbox.lastm = msg
+        })
+
+        mbox.on('end', function() {
+            var mailparser = new MailParser();
+                
+            mailparser.on("end", function(mail_object){
+                mapPlainText(mail_object)
+            });
+
+
+            mailparser.write(mbox.lastm)
+            mailparser.end()
+            mbox = {}
+        });
+    })
+}
+
+function mapPlainText(){
+    var sender = mail_object.from[0].address
+    var subject = mail_object.subject
+    var query_string = subject.split("||")[subject.split("||").length-1]
+    var qs = parseQS(query_string)
+
+    if(subject.indexOf("||") != -1)
+        subject = subject.substring(0,subject.lastIndexOf("||"))
+
+    console.log("---")
+    console.log(sender)
+    console.log(subject)
+    console.log(mail_object.to)
+    console.log("---")
+
+    var p = mail_object.to[0].name
+    
+    var message = {}
+    message.to = []
+    message.subject = subject
+    message.body = mail_object.text
+    if(!message.body)
+        message.body = mail_object.html
+
+    if(!p || p == ""){
+        //new conversation (not replying)
+        if(!qs.to || qs.to == "")
+            return
+        
+        Pseudonym.findOne({initiator:sender,receiver:qs.to}).exec(function(err,pseudonym){
+            if(err)
+                throw err
+            if(pseudonym){
+                message.from = pseudonym.initiator_pseudonym 
+                message.to.push(qs.to)
+                return send(message)
+            }
+            else{
+                Pseudonym.findOne({initiator:qs.to,receiver:sender}).exec(function(err,pseudonym){
+                    if(err)
+                        throw err
+                    if(pseudonym){
+                        message.from = pseudonym.receiver_pseudonym
+                        message.to.push(qs.to)
+                        return send(message)
+                    }
+                    else{
+                        var i_pseudo = animal.getId();
+                        var r_pseudo = animal.getId();
+
+                        var newPseudo = new Pseudonym({})
+                        newPseudo.initiator = sender
+                        newPseudo.initiator_pseudonym = i_pseudo
+                        newPseudo.receiver = qs.to
+                        newPseudo.receiver_pseudonym = r_pseudo
+                        newPseudo.save(function(err,nps){
+                            if(err)
+                                throw err
+                            message.from = nps.initiator_pseudonym
+                            message.to.push(qs.to)
+                            return send(message)
+                        })
+                    }
+                })
+            }
+        })
+    }
+    else{
+        //replying
+        if(!p || p == "")
+            return
+
+        Pseudonym.findOne({initiator:sender,receiver_pseudonym:p}).exec(function(err,pseudonym){
+            if(err)
+                throw err
+            if(pseudonym){
+                if(qs.destroy == 'true')
+                    return destroy(pseudonym)
+                message.from = pseudonym.initiator_pseudonym
+                message.to.push(pseudonym.receiver)
+                return send(message)
+            }
+            else{
+                Pseudonym.findOne({receiver:sender,initiator_pseudonym:p}).exec(function(err,pseudonym){
+                    if(err)
+                        throw err
+                    if(pseudonym){
+                        if(qs.destroy == 'true')
+                            return destroy(pseudonym)
+                                        message.from = pseudonym.receiver_pseudonym
+                                                message.to.push(pseudonym.initiator)
+                        return send(message)
+                    }
+                    else{
+                        return //they borked the pseudonym
+                    }
+                })
+            }
+        })
+    }
+}
+
+function parseQS(query_string){
+    var qs = {}
+    var parts = query_string.split(";")
+    parts.forEach(function(p){
+        var keyval = p.split(":")
+        //var val
+        //if(keyval[1].indexOf(",") != -1){
+        //  val = keyval[1].split(",")
+        //}
+        //else
+        //  val = keyval[1]
+        qs[keyval[0]] = keyval[1]
+    })
+
+    return qs
 }
 
 //destroy pseudnym lookup
 function destroy(pseudonym){
-	console.log("hello?")
-	var email1 = pseudonym.receiver
-	var email2 = pseudonym.initiator
-	pseudonym.remove(function(err){
-		send({from:"Ronald Rivest",to:[email1,email2],subject:"Link between " + pseudonym.receiver_pseudonym + " and " + pseudonym.initiator_pseudonym + " destroyed." })
-	})
+    console.log("hello?")
+    var email1 = pseudonym.receiver
+    var email2 = pseudonym.initiator
+    pseudonym.remove(function(err){
+        send({from:"Ronald Rivest",to:[email1,email2],subject:"Link between " + pseudonym.receiver_pseudonym + " and " + pseudonym.initiator_pseudonym + " destroyed." })
+    })
 }
 
 // send mail with defined transport object
 function send(message){
-	console.log(message)
-	var recipients = message.to
+    console.log(message)
+    var recipients = message.to
         recipients.forEach(function(r){
-		var mailOptions = {
+        var mailOptions = {
                         from: message.from + " <rivest@mit.edu>", // sender address
                         replyTo: message.from + ' <bob@ec2-52-11-124-104.us-west-2.compute.amazonaws.com>',
                         to: '', // list of receivers,
@@ -201,9 +210,9 @@ function send(message){
                                 console.log(error);
                         }else{
                                 //console.log('Message sent to ' + member.email + ': ' + info.response);
-                       		//fs.truncate('/var/mail/bob', 0, function(){console.log('done')})
-				console.log("sent") 
-			}
+                            //fs.truncate('/var/mail/bob', 0, function(){console.log('done')})
+                console.log("sent") 
+            }
                 });
         })
 }
